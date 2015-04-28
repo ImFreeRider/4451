443aa8519e6156bae5fb1f2cee8ca33b5c2cc227
ImFreeRider <keith1_leung@yahoo.com.hk>
2015-04-28 18:34:07 +0800

    Hello

---
+++ Assets/Button.js
@@ -0,0 +1,68 @@
+﻿#pragma strict
+var WhatButton: int = 1;
+var object: Transform;
+var SoundError: GameObject;
+var SoundRight: GameObject;
+var Sparks: GameObject;
+
+function Start () {
+	if ( WhatButton == 1){
+		object = GameObject.Find("RedNoteButton").transform;
+	}
+	if ( WhatButton == 2){
+		object = GameObject.Find("YellowNoteButton").transform;
+	}
+	if ( WhatButton == 3){
+		object = GameObject.Find("BlueNoteButton").transform;
+	}
+	if ( WhatButton == 4){
+		object = GameObject.Find("GreenNoteButton").transform;
+	}
+}
+
+function Update () {
+	if (Input.GetButtonDown ("1")) {
+		if ( WhatButton == 1 ) {
+			if (transform.position.y <= object.position.y)
+			{
+				Instantiate(SoundRight, transform.position + Vector3(-1.5, 4, 0), Quaternion.Euler(0,0,0) );
+				Instantiate(Sparks, transform.position + Vector3(-0.8, 0, -2), Quaternion.Euler(0,0,0) );
+				MainCode.score += 100;
+				Destroy(gameObject);
+			}
+		}
+	}
+	if (Input.GetButtonDown ("2")) {
+		if ( WhatButton == 2 ) {
+			if (transform.position.y <= object.position.y)
+			{
+				Instantiate(SoundRight, transform.position + Vector3(-0.5, 4, 0), Quaternion.Euler(0,0,0) );
+				Instantiate(Sparks, transform.position + Vector3(-0.5, 0, -2), Quaternion.Euler(0,0,0) );
+				MainCode.score += 100;
+				Destroy(gameObject);
+			}		
+		}
+	}
+	if (Input.GetButtonDown ("3")) {
+		if ( WhatButton == 3 ) {
+			if (transform.position.y <= object.position.y)
+			{
+				Instantiate(SoundRight, transform.position + Vector3(0.5, 4, 0), Quaternion.Euler(0,0,0) );
+				Instantiate(Sparks, transform.position + Vector3(0.5, 0, -2), Quaternion.Euler(0,0,0) );
+				MainCode.score += 100;
+				Destroy(gameObject);
+			}		
+		}
+	}
+	if (Input.GetButtonDown ("4")) {
+		if ( WhatButton == 4 ) {
+			if (transform.position.y <= object.position.y)
+			{
+				Instantiate(SoundRight, transform.position + Vector3(1.5, 4, 0), Quaternion.Euler(0,0,0) );
+				Instantiate(Sparks, transform.position + Vector3(0.8, -0.5, -2), Quaternion.Euler(0,0,0) );
+				MainCode.score += 100;
+				Destroy(gameObject);
+			}		
+		}
+	}
+}
---
+++ Assets/Play_Song.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+var Redobject : GameObject;
+var Blueobject : GameObject;
+var Greenobject : GameObject;
+var Yellowobject : GameObject;
+var Whatpart: int = 0;
+var Music: AudioClip;
+
+function Start () {
+if (Whatpart<=1){
+	SongStart();
+	//GetComponent.<AudioSource>().PlayOneShot(Music,0.7);
+	//Debug.Log("Song Function Start");
+	return;
+	}
+}
+function SongStart() {
+	yield WaitForSeconds(0.5);
+	Instantiate(Redobject, transform.position + Vector3(-1.5, 4, 0), Quaternion.Euler(0,0,0) );
+	yield WaitForSeconds(0.5);
+	Debug.Log("Note Drop");
+	Instantiate(Yellowobject, transform.position + Vector3(-0.5, 4, 0), Quaternion.Euler(0,0,0) );
+	yield WaitForSeconds(0.5);
+	Debug.Log("Note Drop");
+	Instantiate(Blueobject, transform.position + Vector3(0.5, 4, 0), Quaternion.Euler(0,0,0) );
+	//yield WaitForSeconds(0.5);
+	Instantiate(Greenobject, transform.position + Vector3(1.5, 4, 0), Quaternion.Euler(0,0,0) );
+	//yield WaitForSeconds(0.5);
+	Whatpart+=1;
+	Debug.Log("SongStartEnd");
+	return;
+}
